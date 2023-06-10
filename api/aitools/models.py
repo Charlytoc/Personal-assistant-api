b0858c88b820e723999d83f341ef9ffe02df0b77
Charly <charly.chacon@improvado.io>
2023-06-10 13:00:44 -0500

    init

--- api/aitools/models.py
+++ api/aitools/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Message(models.Model):
+    USER = 'USER'
+    SYSTEM = 'SYSTEM'
+    ASSISTANT = 'ASSISTANT'
+
+
+    ROLE_CHOICES = [
+        (USER, 'User'),
+        (SYSTEM, 'System'),
+        (ASSISTANT, 'Assistant'),
+    ]
+
+    content = models.TextField()
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+    date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.role}: {self.content}"
