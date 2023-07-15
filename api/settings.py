6a9043101323ef203ab1f3a8148cbc9bf1314c1e
Charly Chacon <charlyjchaconc@gmail.com>
2023-07-15 16:54:16 -0500

    add render.yaml

--- api/settings.py
+++ api/settings.py
@@ -194,3 +194,8 @@ LOGGING = {
         'level': 'INFO',  # Or whatever level you want
     },
 }
+
+
+# CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL')
+# CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
